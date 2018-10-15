3f93976d2d95c832c91a13b9decd680772a3d0ae
Dhaval Patel <dpate236@uic.edu>
2018-10-15 17:23:20 -0500

    Exit command is working

--- hw3.c
+++ hw3.c
@@ -2,27 +2,27 @@
 #include <string.h>
 #include <unistd.h>
 int main(){
+ char line[500];
+ char argsarray[20][100];
  while(1){
   //create some space for our strings
-  char line[500];
-  char argsarray[20][100];
-
   //print prompt
   //read line from terminal
    printf("CS361 >:");
-  fgets(line, 500, stdin);  
+ fgets(line, 500, stdin);   
   //break the string up into words
-  char *word = strtok(line, " ");
+  char *word = strtok(line, " \n");
   int i = 0;
 
-  
   while (word) {
     printf("word: %s\n",word);
     //copy a word to the arg array
     strcpy(argsarray[i], word);
     //get next word
     word = strtok(NULL, " ");
-    i = i + 1;
+    i = i + 1; 
   }
- }
+  if(strcmp(argsarray[0],"exit") == 0)
+    return 0;
+ } 
 }
